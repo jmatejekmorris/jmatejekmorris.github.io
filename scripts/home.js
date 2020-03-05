2c4e7f8ba06dffb906383d944a75292c5691bdae
Brian Matejek <bmatejek@gmail.com>
2020-03-05 02:41:44 -0500

    home page: initial commit

---
+++ scripts/home.js
@@ -0,0 +1,11 @@
+function ExpandBooks() {
+	var book_items = document.getElementsByClassName('menu-item-book');
+	for (var i = 0; i < book_items.length; ++i) {
+		if (book_items[i].style.display == 'block') {
+			book_items[i].style.display = 'none';
+		}
+		else {
+			book_items[i].style.display = 'block';
+		}
+	}
+}
