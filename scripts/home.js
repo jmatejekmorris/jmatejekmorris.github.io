457133b228d4dcea83befa968e29653aabc089b7
Brian Matejek <bmatejek@gmail.com>
2020-08-27 16:36:16 -0400

    home: added +/- sign for books div

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,15 @@
 function ExpandBooks() {
+	var book_menu_div = document.getElementById('list-books-menu-option');
+
 	var book_items = document.getElementsByClassName('menu-item-book');
 	for (var i = 0; i < book_items.length; ++i) {
 		if (book_items[i].style.display == 'block') {
 			book_items[i].style.display = 'none';
+			book_menu_div.style.backgroundImage = "url('/images/books-menu-home-open.png')";
 		}
 		else {
 			book_items[i].style.display = 'block';
-
-      book_items[i].classList.add('blur');
+			book_menu_div.style.backgroundImage = "url('/images/books-menu-home-closed.png')";
 		}
 	}
 }
