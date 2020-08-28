cd697ff9fbcaa1bed90d2d83a1ac6440a92f89d7
Brian Matejek <bmatejek@gmail.com>
2020-08-28 14:19:35 -0400

    home: updated book menu to slowly toggle display and none

--- scripts/home.js
+++ scripts/home.js
@@ -1,15 +1,14 @@
 function ExpandBooks() {
-	var book_menu_div = document.getElementById('list-books-menu-option');
+    var book_menu_div = document.getElementById('list-books-menu-option');
 
-	var book_items = document.getElementsByClassName('menu-item-book');
-	for (var i = 0; i < book_items.length; ++i) {
-		if (book_items[i].style.display == 'block') {
-			book_items[i].style.display = 'none';
-			book_menu_div.style.backgroundImage = "url('/images/books-menu-home-open.png')";
-		}
-		else {
-			book_items[i].style.display = 'block';
-			book_menu_div.style.backgroundImage = "url('/images/books-menu-home-closed.png')";
-		}
-	}
+    var books_container_div = document.getElementById('books-container-div');
+
+    if (books_container_div.style.display == 'block') {
+        book_menu_div.style.backgroundImage = "url('/images/books-menu-home-open.png')";
+    }
+    else {
+        book_menu_div.style.backgroundImage = "url('/images/books-menu-home-closed.png')";  
+    }
+
+    $('#books-container-div').slideToggle();
 }
