e83924d1545e8ba287a44882458f4b5ae9d43034
Brian Matejek <bmatejek@gmail.com>
2020-08-28 11:55:09 -0400

    content-div: created event listener to reach bottom of page

---
+++ scripts/default.js
@@ -0,0 +1,12 @@
+window.onload = updateContentDiv();
+
+window.addEventListener('resize', updateContentDiv);
+
+
+
+function updateContentDiv() {
+	var height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+	var content = document.getElementById('content');
+	content.style.height = height;
+}
