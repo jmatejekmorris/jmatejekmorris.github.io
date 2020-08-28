ea7ba200cd48cf59eb9989efdd23ffe47796d8d4
Brian Matejek <bmatejek@gmail.com>
2020-08-28 16:46:28 -0400

    fixed android vertical bar bug

--- scripts/default.js
+++ scripts/default.js
@@ -1,3 +1,8 @@
+window.onresize = function() {
+	var content = document.getElementById('content');
+	content.style.minHeight = '100vh';
+}
+
 function ExpandTopMenuBooks() {
     $('#books-menu-default').slideToggle(150);
 }
