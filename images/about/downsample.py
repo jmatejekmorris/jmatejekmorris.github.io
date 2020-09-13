215baaf6045964ce65f9f9a24795b32861b98ad8
Brian Matejek <bmatejek@gmail.com>
2020-09-13 10:56:46 -0400

    downsampled about me images

---
+++ images/about/downsample.py
@@ -0,0 +1,22 @@
+import os
+import glob
+
+
+from skimage import io, transform
+
+
+
+if not os.path.exists('full-resolution-images'):
+    os.mkdir('full-resolution-images')
+
+filenames = glob.glob('*jpeg')
+
+for filename in filenames:
+    image = io.imread(filename)
+
+    new_filename = 'full-resolution-images/{}'.format(filename)
+    os.rename(filename, new_filename)
+
+    image_resized = transform.resize(image, (400, 400))
+
+    io.imsave(filename, image_resized)
